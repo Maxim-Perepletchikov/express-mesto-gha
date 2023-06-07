be9c6432822e71976f626e416c554869c0fb215e
Maxim Perepletchikov <maxwell-38@yandex.ru>
2023-06-07 10:36:18 +0500

    fixed: errors

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,10 @@ const router = require("express").Router();
 const userRoutes = require("./users");
 const cardRoutes = require("./cards");
 
-router.use("/*", (req, res) =>
-  res.status(404).send({ message: "Страница не найдена" })
-);
 router.use(userRoutes);
 router.use(cardRoutes);
+router.use("/*", (req, res) =>
+res.status(404).send({ message: "Страница не найдена" })
+);
 
 module.exports = router;
