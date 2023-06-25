79553096190214bc8151b09b28b851a04ae4dd39
Maxim Perepletchikov <maxwell-38@yandex.ru>
2023-06-25 21:29:11 +0500

    fixed

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const { createUser, login } = require('../controllers/users');
 const auth = require('../middlewares/auth');
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
 
-router.post('/signup', createUser);
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(/https?:\/\/(www)?[0-9a-z\-._~:/?#[\]@!$&'()*+,;=]+#?$/i),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
 router.post('/signin', login);
 
 router.use(auth);
