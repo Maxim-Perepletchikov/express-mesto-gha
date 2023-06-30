013d76b80d798ba800ce83a3a3981ff7a2941489
Maxim Perepletchikov <maxwell-38@yandex.ru>
2023-06-30 19:53:32 +0500

    feat: logger

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const { errors } = require('celebrate');
 const helmet = require('helmet');
 const router = require('./routes');
 const { errorHandler } = require('./middlewares/error');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const limiter = rateLimit({
@@ -25,8 +26,11 @@ app.use(limiter);
 app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
+app.use(requestLogger);
 
 app.use(router);
+
+app.use(errorLogger);
 
 app.use(errors());
 
